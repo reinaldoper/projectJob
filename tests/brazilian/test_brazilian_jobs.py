50df3fca9291516e93c382c95501388e94a8ac2b
reinaldoper <reinaldoper83@gmail.com>
2023-03-25 15:45:01 -0400

    feat: adidiona requisito 11

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,12 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+
+    jobs = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    list_item = []
+    for job in jobs:
+        list_item.append(set(job.keys()))
+    for keys in list_item:
+        for key in list(keys):
+            assert key in ["title", "salary", "type"]
