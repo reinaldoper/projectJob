1ca8df3436d143da2b361862e1462c0ed0804eed
reinaldoper <reinaldoper83@gmail.com>
2023-03-25 15:16:47 -0400

    feat: adiciona requisito 10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,19 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
+from unittest.mock import mock_open, patch
+import pytest
 
 
-def test_counter():
-    pass
+@pytest.fixture
+def read():
+    return """
+    Linguagem de Programação é uma linguagem escrita e formal
+    que especifica um conjunto de instruções e regras usadaspara
+    gerar programas (software). Um software pode ser desenvolvido
+    para rodar em um computador, dispositivo móvel
+    ou em qualquer equipamento que permita sua execução.
+    """
+
+
+def test_counter(read):
+    with patch("builtins.open", mock_open(read_data=read)):
+        assert count_ocurrences("path/path", "linguagem") == 2
